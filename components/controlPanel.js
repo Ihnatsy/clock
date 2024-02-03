701230fef9fbc1a9ba12fc96d5745e8171b1703f
Ihnatsy <ihpo5550@gmail.com>
2024-02-03 19:33:00 +0300

    render clock by JS

--- components/controlPanel.js
+++ components/controlPanel.js
@@ -5,7 +5,9 @@ export function buttonsPanel() {
     const controlsBtns = document.createElement('div')
     controlsBtns.className = "buttonCase"
     const startButton = ControlButtonElement('Запустить часы', startClock)
+    startButton.id = "start"
     const stopButton = ControlButtonElement('Остановить часы', stopClock)
+    stopButton.id = "stop"
     controlsBtns.append(startButton, stopButton)
     return controlsBtns
 }
